786717bb3326681dbaa9499cd85651e374ad74b5
tim-scharf <timothy.scharf@uptake.com>
2016-02-28 20:29:25 -0600

    directory changes

--- telestra/getDat0.R
+++ telestra/getDat0.R
@@ -2,8 +2,8 @@ require(data.table)
 require(entropy)
 
 setwd('~/repos/scharf-personal/telestra/')
-X0 <- fread('~/data/telestra/train.csv')
-X1 <- fread('~/data/telestra/test.csv')
+X0 <- fread('data/train.csv')
+X1 <- fread('data/test.csv')
 X <- rbindlist(list(X0,X1),fill = T)
 setkey(X,id)
 idx <- which(!is.na(X$fault_severity))
@@ -21,21 +21,21 @@ X2[,(y_prop):= .SD/(sum(.SD)),by = location]
 X2[,entrpy:=apply(.SD,1,entropy),.SDcols=fac_cols,by = location]
 setkey(X2,location)
 
-logs <- fread('~/data/telestra/log_feature.csv')
+logs <- fread('data/log_feature.csv')
 setkey(logs, id,log_feature)
 L <- dcast(logs, id~log_feature,value.var = 'volume',fill = 0)
 
-severity_type <- fread('~/data/telestra/severity_type.csv')
+severity_type <- fread('data/severity_type.csv')
 severity_type[,V1:=1]
 S <- dcast(severity_type, id~severity_type, value.var ='V1',fill = 0)
 setkey(S,id)
 
-events <- fread('~/data/telestra/event_type.csv')
+events <- fread('data/event_type.csv')
 events[,V1 :=1]
 setkey(events,id, event_type)
 E <- dcast(events, id ~ event_type, value.var = 'V1', fill = 0)
 
-resource <- fread('~/data/telestra/resource_type.csv')
+resource <- fread('data/resource_type.csv')
 resource[,V1:=1]
 setkey(resource, id, resource_type)
 R <- dcast(resource, id~ resource_type, value.var = 'V1', fill = 0)
