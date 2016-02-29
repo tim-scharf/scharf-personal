b779d48b075c3e20ca02bbcbf7008dc22694d3e2
Timothy Scharf <timothy.scharf@uptake.com>
2016-02-28 20:26:54 -0600

    data add

--- telestra/tsne.R
+++ telestra/tsne.R
@@ -10,33 +10,40 @@ grid = expand.grid(PCA = c(T,F),
                    scale_it = c(T,F),
                    log_it = c(T,F))
 
-results <- vector(mode='list', length = nrow(grid))
+TSNE_list <- list()
 
 for(i in 1:nrow(grid)){
-   M0 <- M
-  if(grid[i,'log_it'])   M0 <- log1p(M0)
-  if(grid[i,'scale_it']) M0 <- scale(M0)
-  
-results[[i]] <- Rtsne(M0, 
-                      dims = 2, 
-                      initial_dims = 75, 
-                      perplexity = grid[i,'perplexity'], 
-                      theta = .5,
-                      pca = grid[i,'PCA'],
-                      verbose = T,
-                      max_iter = 2000)
+   PCA <- grid[i,'PCA']
+   perplexity <- grid[i,'perplexity']
+   scale_it <- grid[i,'scale_it']
+   log_it <- grid[i,'log_it']
+   
+   data <- copy(M)
+   if(log_it) data <- log1p(data)
+   if(scale_it) data <- scale(data)
+   print(i)
+   TSNE_list[[i]] <- Rtsne(X=data,
+                 perplexity = perplexity,
+                 pca = PCA,
+                 theta = .5,
+                 verbose = T,
+                 max_iter = 1500,
+                 dims = 2,
+                 initial_dims = 75)
+  saveRDS(TSNE_list,'TSNE_list.rds')
 }
 
-plot(TSNE_raw$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
-plot(TSNE_log1p$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
-plot(TSNE_log1p$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
-plot(TSNE_raw_scale$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
-plot(TSNE_raw_scale_noPCA$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
-plot(TSNE_raw_noPCA$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
 
-
-plot(TSNE_raw_scale_noPCA$Y[idx,2],TSNE_raw_scale$Y[idx,1] ,pch = 19 , cex = .5, col= X$y[idx])
-
-require(car)
-scatter3d(x=TSNE_raw3$Y[idx,1],y=TSNE_raw3$Y[idx,2],z=TSNE_raw3$Y[idx,3],surface =F,
-          point.col = as.numeric(X$y[idx]))
+# plot(TSNE$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# plot(TSNE_log1p$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# plot(TSNE_log1p$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# plot(TSNE_raw_scale$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# plot(TSNE_raw_scale_noPCA$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# plot(TSNE_raw_noPCA$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
+# 
+# 
+# plot(TSNE_raw_scale_noPCA$Y[idx,2],TSNE_raw_scale$Y[idx,1] ,pch = 19 , cex = .5, col= X$y[idx])
+# 
+# require(car)
+# scatter3d(x=TSNE_raw3$Y[idx,1],y=TSNE_raw3$Y[idx,2],z=TSNE_raw3$Y[idx,3],surface =F,
+#           point.col = as.numeric(X$y[idx]))
