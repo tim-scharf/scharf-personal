bea384438dc29a7f825cde50f2b5ad01e3f26f6b
tim-scharf <timothy.scharf@uptake.com>
2016-02-29 06:47:17 -0600

    rds files

--- telestra/tsne.R
+++ telestra/tsne.R
@@ -1,18 +1,22 @@
 require(Rtsne)
+require(doMC)
+source('getDat0.R')
+
 set.seed(1234)
+registerDoMC(detectCores()-1)
 DT <- L[S[E[R]]]
 DT[,noise:=runif(nrow(DT))]
 
 M <- as.matrix(DT[,-'id',with=F])
 
 grid = expand.grid(PCA = c(T,F), 
-                   perplexity = c(15,20,25,30,35),
+                   perplexity = c(5,10,15,20,25,30,35,40,45),
                    scale_it = c(T,F),
                    log_it = c(T,F))
 
-TSNE_list <- list()
 
-for(i in 1:nrow(grid)){
+
+A <- foreach(i=1:nrow(grid),.verbose=T) %dopar%{
    PCA <- grid[i,'PCA']
    perplexity <- grid[i,'perplexity']
    scale_it <- grid[i,'scale_it']
@@ -22,17 +26,17 @@ for(i in 1:nrow(grid)){
    if(log_it) data <- log1p(data)
    if(scale_it) data <- scale(data)
    print(i)
-   TSNE_list[[i]] <- Rtsne(X=data,
+   z <-          Rtsne(X=data,
                  perplexity = perplexity,
                  pca = PCA,
                  theta = .5,
                  verbose = T,
-                 max_iter = 1500,
+                 max_iter = 2000,
                  dims = 2,
                  initial_dims = 75)
-  saveRDS(TSNE_list,'TSNE_list.rds')
+  z
 }
-
+saveRDS(A,'TSNE_list.rds')
 
 # plot(TSNE$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
 # plot(TSNE_log1p$Y[idx,],pch = 19 , cex = .5, col= X$y[idx])
