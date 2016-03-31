1ef736ecc1c95ea33feaea775a1d4de85f9feaba
tim-scharf <timothy.scharf@uptake.com>
2016-03-31 16:19:29 -0500

    do it

--- numerai/getData0.R
+++ numerai/getData0.R
@@ -3,42 +3,28 @@ require(Matrix)
 require(xgboost)
 require(methods)
 require(utils)
-require(FastImputation)
 
-setwd('~/repos/scharf-personal/BNP/')
+setwd('~/repos/scharf-personal/numerai/')
 
-X <- rbindlist(list(fread('~/data/BNP/train.csv'),fread('~/data/BNP/test.csv')),fill = T)
+X0 <-  fread('data/numerai_training_data.csv')
+X1 <-  fread('data/numerai_tournament_data.csv')
 
-train_idx <- which(!is.na(X$target))
-test_idx <- which(is.na(X$target))
-data_cols <- paste0('v',1:131)
-fac_cols  <- names(X)[sapply(X,is.character)]
-num_cols <- setdiff(data_cols,fac_cols)
-
-MFAC <- treat_factors(X,fac_cols = fac_cols,max_cat = 2**20)
+data_cols <- paste0('feature',1:21)
 
 
-Xmed <- replaceNA_med(X[,num_cols,with=F])
-X999 <- replaceNA(X[,num_cols,with=F],-999)
-
-M999 <- as(as.matrix(X999), "sparseMatrix")
-Mmed <- as(as.matrix(Xmed), 'sparseMatrix')
-
-M <- cBind(M999,MFAC)
-M_med <- cbind(Mmed,MFAC)
-
-Xtrain <- M_med[train_idx,]
-Xtest <- M_med[test_idx,]
+#z <- sapply(data_cols,function(col)  t.test(X0[,get(col)], X1[,get(col)]))
 
 
 
 
-y <- X[train_idx,target]
+Xtrain <- as.matrix(X0[,mget(data_cols)])
+Xtest <-  as.matrix(X1[,mget(data_cols)])
+y <- X0[,target]
 
 attr(Xtrain,'missing') <- ('no_missing')
-saveRDS(Xtrain,'data/Xtrain_median_imp.rds')
-saveRDS(Xtest,'data/Xtest_median_imp.rds')
-saveRDS(y,'data/y.rds')
+saveRDS(Xtrain,'data/Xtrain.rds')
+saveRDS(Xtest,'data/Xtest.rds')
+saveRDS(as.numeric(y),'data/y.rds')
 
 
 
